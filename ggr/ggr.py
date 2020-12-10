559617287b2967c262b648b8839c8b0bcd87e68c
Mario Jason Braganza <jason@janusworx.com>
2020-12-10 16:57:10 +0530

    trying to sanitise url. saving current work
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- ggr/ggr.py
+++ ggr/ggr.py
@@ -1,21 +1,39 @@
 import argparse
+import re
+
 import requests
 
 
 def ggr():
-    get_url()
+    gru = get_url().strip()
+    parse_gl_url(gru)
 
 
 def get_url():
+    """
+    Gets and returns a url from the prompt
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "url",
         type=str,
-        metavar="https://gitlab.com/{repo}/{project}/-/releases",
-        help="Please type or paste in a link to a complete Gitlab releases url",
+        metavar="https://gitlab.com/{some-repo}/{some-project}/-/releases",
+        help="Please give a link to a complete Gitlab releases url",
     )
     args = parser.parse_args()
-    print(type(args.url))
+    return args.url
+
+
+def parse_gl_url(url):
+
+    r = requests.get(url)
+    try:
+        if not r.raise_for_status():
+            print(r.text)
+        else:
+            print(f"Something’s off. Please check the url, {url}")
+    except requests.exceptions.HTTPError:
+        print(f"Something’s off. Please check the url you typed in - {url}")
 
 
 if __name__ == "__main__":
