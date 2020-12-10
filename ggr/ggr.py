cde8718f20c9c73addb2694d5a6a9fc69556a767
Mario Jason Braganza <jason@janusworx.com>
2020-12-10 11:14:12 +0530

    working on getting a url
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- ggr/ggr.py
+++ ggr/ggr.py
@@ -1,8 +1,21 @@
-import sys
+import argparse
+import requests
 
 
 def ggr():
-    print("hello world")
+    get_url()
+
+
+def get_url():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "url",
+        type=str,
+        metavar="https://gitlab.com/{repo}/{project}/-/releases",
+        help="Please type or paste in a link to a complete Gitlab releases url",
+    )
+    args = parser.parse_args()
+    print(type(args.url))
 
 
 if __name__ == "__main__":
