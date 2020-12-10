ea8bc5a238da68919327f79a8815e9025ed86da8
Mario Jason Braganza <jason@janusworx.com>
2020-12-10 18:50:11 +0530

    finally managed to get a proper-ish url and then get it contents
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- ggr/ggr.py
+++ ggr/ggr.py
@@ -2,11 +2,16 @@ import argparse
 import re
 
 import requests
+from bs4 import BeautifulSoup
+
+gen_err_msg = "Something’s off. Please check the url you typed in - "
 
 
 def ggr():
-    gru = get_url().strip()
-    parse_gl_url(gru)
+    gru = get_url()
+    parsed_raw_release = parse_gl_url(gru)
+    if parsed_raw_release:
+        print(parsed_raw_release)
 
 
 def get_url():
@@ -25,15 +30,19 @@ def get_url():
 
 
 def parse_gl_url(url):
-
-    r = requests.get(url)
-    try:
-        if not r.raise_for_status():
-            print(r.text)
-        else:
-            print(f"Something’s off. Please check the url, {url}")
-    except requests.exceptions.HTTPError:
-        print(f"Something’s off. Please check the url you typed in - {url}")
+    gl_rl_page_regex_pattern = re.compile(r"https://gitlab.com/.*?/.*?/-/releases")
+    url_after_check = gl_rl_page_regex_pattern.search(url)
+    if url_after_check:
+        r = requests.get(url)
+        try:
+            if not r.raise_for_status():
+                return r.text
+            else:
+                print(f"{gen_err_msg}{url}")
+        except requests.exceptions.HTTPError:
+            print(f"{gen_err_msg}{url}")
+    else:
+        print(f"{gen_err_msg}{url}")
 
 
 if __name__ == "__main__":
